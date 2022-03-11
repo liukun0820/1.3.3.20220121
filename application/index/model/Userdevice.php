c528cfe9685ddabd1b2c2276c27f92cc62f4f0e9
liukun0820 <haonan0820@163.com>
2022-03-11 13:34:36 +0800

    模型的获取器和修改器，获取原始数据的方法

--- application/index/model/Userdevice.php
+++ application/index/model/Userdevice.php
@@ -11,6 +11,7 @@ class Userdevice extends Model
 	//protected $table = 'fa_userdevice';
 	
 	// 获取器的作用是在获取数据的字段值后自动进行处理，例如，我们需要对状态值进行转换
+	// 如果想获取原始字段数据 使用getData('status')或者getData()
 	public function getIsDisableAttr($value)
     {
         $status = [0=>'禁用',1=>'正常'];
@@ -18,6 +19,7 @@ class Userdevice extends Model
     }
 	
 	// 修改器的作用是可以在数据赋值的时候自动进行转换处理
+	// 如果想获取原始字段数据 使用getData('status')或者getData()
 	public function setSnAttr($value)
     {
         return strtoupper($value); // 保存数据的时候，自动转换成大写
