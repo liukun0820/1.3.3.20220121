86be97f0e63e8250fa9cd2414a757d83c6e510d3
liukun0820 <haonan0820@163.com>
2022-03-11 13:09:43 +0800

    使用模型的获取器

--- application/index/model/Userdevice.php
+++ application/index/model/Userdevice.php
@@ -10,6 +10,13 @@ class Userdevice extends Model
     protected $name = 'userdevice';
 	//protected $table = 'fa_userdevice';
 	
+	// 获取器的作用是在获取数据的字段值后自动进行处理，例如，我们需要对状态值进行转换
+	public function getIsDisableAttr($value)
+    {
+        $status = [0=>'禁用',1=>'正常'];
+        return $status[$value];
+    }
+	
 	/* public function user()
 	{
 		return $this->belongsTo('User', 'user_id', 'id');
